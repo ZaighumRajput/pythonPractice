b922c059c0d8fd1359abf1073542b57ceaea2252
zaighumrajput <zaighum47@gmail.com>
2016-05-12 14:03:02 +0000

    Start of birthday calc

---
+++ birthdayApp/birthdayCalc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def header(character, lengthOfLines, appName):
+    header = character*lengthOfLines + "\n\t" +  appName + "\n" + character*lengthOfLines
+    return header 
+
+
+def BirthdayApp()
+    print(header("-", 30, "BIRTHDAY APP"))
+
+    BirthdayYear = int(input(Whay year you born [YYYY]?))
+    BirthdayMonth = int(input(What month were you born [MM]?))
+    BirthdayDay = int(input(What day were you born [DD]?))
+
+    
+
+
+if __name__ == "__main__":
