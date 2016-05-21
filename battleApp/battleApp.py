af78ba6d1b0e6f919fe55cdc0a05c65e2440066e
zaighumrajput <zaighum47@gmail.com>
2016-05-21 23:17:07 +0000

    some bugs

--- battleApp/battleApp.py
+++ battleApp/battleApp.py
@@ -1,46 +1,93 @@
 #!/usr/bin/python3
 import random
-from characters import Soldier, Creature
+from characters import Soldier, Creature, Dragon, HealthBoost, StrengthBoost
 
 
 def main():
     #print header
     #initialize actors and map
-    creatures = [
-            Creature('Toad', 1),
-            Creature("Warrior", 75),
-            Creature("Tiger", 10),
-            Creature("Dragon", 100)
+
+    #game loop
+    game_loop()
+
+def game_loop():
+    '''Main game loop'''
+    AllCreatures = [
+            Creature('Toad', 1, 5),
+            Creature("Warrior", 75, 100),
+            Creature("Tiger", 10, 200),
+            Dragon("Dragon", 100,200, 20, False),
+            Dragon("Dragon", 100,200, 40, True)
     ]
 
-    print(creatures)
+    AllPowerups = [
+            HealthBoost("Health", 500),
+            StrengthBoost("Milk", 10),
+            StrengthBoost("Steroids", 100),
+            StrengthBoost("HGH", 1000),
+            HealthBoost("Dinner", 1000)
+    ]
 
-    hero = Soldier('You', 78)
-    #game loop
-    game_loop(hero, creatures)
-
-def game_loop(hero, creatures):
-    '''Main game loop'''
+    hero = Soldier('You', 1000, 150)
 
     while True:
-        active_creature = random.choice(creatures) 
+        active_creature = random.choice(AllCreatures) 
         print("{} of health {} appears!".format(active_creature.name, active_creature.health))
         cmd = input("What would you like to do? [A]ttack, [R]unaway, [L]ookaround: ")
         cmd = cmd.upper()
 
         if(cmd == 'A'):
-            print("Attacking!")
-            hero.attack(active_creature)
+            attack_sequence(hero, active_creature, AllCreatures)
+            if(hero.health <  0): break
         elif(cmd == 'R'):
-            print("Running away...")
+            running_sequence(hero, AllPowerups)
         elif(cmd == 'L'):
-            print("Looking around")
+            looking_sequence(AllCreatures)
         else:
             confirmation = input("E[X]it the game? Any other key to continue: ")
             if confirmation.upper() == 'X':
                 break
 
+def powerdrop(hero, AllPowerups):
 
+    if(AllPowerups):
+        print("dropping powerup")
+        random.shuffle(AllPowerups)
+        hero.use_powerup(AllPowerups.pop())
+    else:
+        return None
+
+def running_sequence(hero, AllPowerups):
+    powerdrop(hero, AllPowerups)
+    print("You ran away")
+
+
+
+
+
+
+def looking_sequence(AllCreatures):
+    print("Looking around...")
+    for c in AllCreatures:
+        print("* {} with {} health".format(c.name, c.health))
+
+def attack_sequence(hero, creature, AllCreatures):
+    while hero.health > 0:
+        print("You attacked {}".format(creature.name))
+        hero.attack(creature)
+        if(creature.health < 0):
+            print("You defeated {}".format(creature.name))
+            AllCreatures.remove(creature)
+            return None
+        elif(hero.health > 0):
+            print("Your health is at {}. {} is at {}".format(hero.health, creature.name, creature.health))
+            cmd = input("Attack again? [Y]")
+            if cmd.upper() == 'Y':
+                continue
+            else:
+                print("You fled the battle scene")
+                return None
+    print("You died!..")
 
 if __name__ == "__main__":
     main()
--- battleApp/characters.py
+++ battleApp/characters.py
@@ -1,25 +1,69 @@
 import random
 
 class Creature:
-    def __init__(self, name : str, health: int):
+    def __init__(self, name : str, health: int, strength: int):
         self.name = name
         self.health = health 
+        self.strength = strength
 
     def defensive_roll(self):
-        return random.randint(1,12) * self.health 
+        return random.randint(1,12) * self.health *self.strength 
+
+    def use_powerup(self, Powerups):
+        print("using powerup")
+        if Powerups == HealthBoost:
+            self.health += Powerups.power()
+            print("You used {}, your health is now {}".format(Powerups.name, self.health))
+        elif Powerups == StrengthBoost:
+            self.strength *= Powerups.power()
+            print("You used {], your strength is now {}".format(Powerups.name, self.strength))
+
 
     def attack(self, Creature):
         my_roll = self.defensive_roll()
         other_roll = Creature.defensive_roll()
         if (my_roll > other_roll):
             Creature.damage(my_roll)
+            print("You dealt {} damage!".format(my_roll))
         else:
             self.damage(other_roll/2)
+            print("You got hurt -{}".format(other_roll/2))
 
     def damage(self, damage : int):
         self.health = self.health - damage
+
     def __repr__(self):
         return "{}: health {}".format(self.name, self.health)
 
 class Soldier(Creature):
     pass
+
+class Dragon(Creature):
+    def __init__(self, name, health,strength, scaliness, can_breath_fire):
+        super().__init__(name, health, strength)
+        self.scaliness = scaliness
+        self.can_breath_fire = can_breath_fire
+
+    def defensive_roll(self):
+        return super().defensive_roll() if (not self.can_breath_fire) else super().defensive_roll() * self.scaliness
+    
+
+class Powerups():
+    def __init__(self, name):
+        self.name = name
+
+class HealthBoost(Powerups):
+    def __init__(self, name, power):
+        super().__init__(name)
+        self.power = power
+    
+    def __repr__(self):
+        return ("Health boost of {}".format(self.power))
+
+class StrengthBoost(Powerups):
+    def __init__(self,name, power):
+        super().__init__(name)
+        self.power = power
+
+    def __repr__(self):
+        return ("Strength boos of {}".format(self.power))
