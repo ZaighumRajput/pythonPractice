0a43d46e3249f0a4204476051d502de3abe51adf
zaighumrajput <zaighum47@gmail.com>
2016-05-21 16:54:26 +0000

    Unicode error

--- weatherApp/weatherApp.py
+++ weatherApp/weatherApp.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3
+#!\usr\bin\python3
+
 import requests
 from bs4 import BeautifulSoup
+import collections
+
+WeatherReport = collections.namedtuple('WeatherReport', 
+                                        ['condition', 'temperature', 'scale', 'location'])
+
 
 def print_header():
     pass
@@ -11,24 +17,49 @@ def get_zipcode():
 
 def get_html_from_web(zipcode):
     url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
+
     response = requests.get(url)
-    return response.text
+    if response.status_code == 200:
+        return response.text
+    return None 
 
 def get_weather_from_html(html_doc):
-	soup = BeautifulSoup(html_doc, 'html.parser')
-	location = soup.find(id='location').find('h1').get_text()
-	temperature = soup.find(id='curCond').find(class_='wx_value').get_text()
-	#scale = 
-	#condition = 
-	print(temperature)
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    loc = soup.find(id='location').find('h1').get_text()
+    condition = soup.find(id='curCond').find(class_='wx-value').get_text()
+    temp = soup.find(id='curTemp').find(class_='wx-value').get_text()
+    scale = soup.find(id='curTemp').find(class_='wx-unit').get_text()
+    
+    #reall necessary to repeat this 4 times?
+    loc = clean_up_text(loc)
+    condition = clean_up_text(condition)
+    temp = clean_up_text(temp)
+    scale = clean_up_text(scale)
+    
+    report = WeatherReport(location=loc, condition=condition, temperature=temp, scale=scale)
+
+    return report
+
+
+def clean_up_text(text : str):
+    if not text:
+        return text
+    
+    return text.strip()
 
 def WeatherApp():
     #print the header
     html = get_html_from_web(get_zipcode())
 	    
     #parse the html
-    get_weather_from_html(html)
+    if html:
+        report = get_weather_from_html(html)
+    else:
+        print("Could not connect")
     #display for the forecast
+    
+    print(report)
 
 WeatherApp()
 
